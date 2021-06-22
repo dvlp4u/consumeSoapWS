ae336888bac34934fb8145c54921704b38933869
slamalfa <slamalfa@imolainformatica.it>
2021-06-22 16:48:10 +0200

    Creato Client Soap

---
+++ src/main/java/it/dvlpr4u/exercise/ConsumeSoapWS/Client/BibliotecaClient.java
@@ -0,0 +1,2 @@
+package it.dvlpr4u.exercise.ConsumeSoapWS.Client;public class BibliotecaClient {
+}
---
+++ src/main/java/it/dvlpr4u/exercise/ConsumeSoapWS/Configuration/BibliotecaConfig.java
@@ -0,0 +1,2 @@
+package it.dvlpr4u.exercise.ConsumeSoapWS.Configuration;public class BibliotecaConfig {
+}
