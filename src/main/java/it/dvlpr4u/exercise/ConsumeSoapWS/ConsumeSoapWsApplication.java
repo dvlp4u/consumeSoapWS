3165fd09e386ed8ba3b048b046fbe386d4f5eb22
slamalfa <slamalfa@imolainformatica.it>
2021-06-22 15:19:04 +0200

    first commit

---
+++ src/main/java/it/dvlpr4u/exercise/ConsumeSoapWS/ConsumeSoapWsApplication.java
@@ -0,0 +1,13 @@
+package it.dvlpr4u.exercise.ConsumeSoapWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeSoapWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/dvlpr4u/exercise/ConsumeSoapWS/ConsumeSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dvlpr4u.exercise.ConsumeSoapWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
